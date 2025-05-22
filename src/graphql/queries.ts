ff1db95124dea49bfa505dd4d13dfdcedb8d3aa7
Zeeshan1027 <zeeshan22ahmad@gmail.com>
2025-05-22 13:03:01 +0530

    adding the final changes

---
+++ src/graphql/queries.ts
@@ -0,0 +1,37 @@
+import gql from "graphql-tag";
+
+export const USERS_SELECT_QUERY = gql`
+  query UsersSelect(
+    $filter: UserFilter!
+    $sorting: [UserSort!]
+    $paging: OffsetPaging!
+  ) {
+    users(filter: $filter, sorting: $sorting, paging: $paging) {
+      totalCount
+      nodes {
+        id
+        name
+        avatarUrl
+        email
+        phone
+        jobTitle
+      }
+    }
+  }
+`;
+
+export const TASK_STAGES_SELECT_QUERY = gql`
+  query TaskStagesSelect(
+    $filter: TaskStageFilter!
+    $sorting: [TaskStageSort!]
+    $paging: OffsetPaging!
+  ) {
+    taskStages(filter: $filter, sorting: $sorting, paging: $paging) {
+      totalCount
+      nodes {
+        id
+        title
+      }
+    }
+  }
+`;
